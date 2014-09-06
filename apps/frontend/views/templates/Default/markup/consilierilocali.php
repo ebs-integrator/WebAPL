1dd939e02f480c0cbdf34a34ac252086e77a08f3
Aliona Levcenco <alyona_0594@mail.ru>
2014-09-06 17:51:56 +0300

    no message

---
+++ apps/frontend/views/templates/Default/markup/consilierilocali.php
@@ -0,0 +1,59 @@
+<section>
+    <div class="dirs_menu">
+        <div class="wrap">
+            <a href="javascript:;">Principala »</a>
+            <a href="javascript:;">Consiliul local »</a>
+            <a href="javascript:;">Consiliul local »</a>
+            <a href="javascript:;">Consilierii locali și domeniile de responsabilitate</a>
+        </div>
+    </div>
+    <div class="wrap">
+        <p class="c_title">Consilieri locali</p>
+        <div class='left'>
+            <table class="reg_tb">
+                <thead>
+                    <tr><td>Numele, prenumele</td><td>Apartenenta politica</td><td>contacte</td><td>Sectorul</td></tr>
+                </thead>
+                <tbody>
+                    <tr><td>Scripnic Ivan</td><td>Partidul Comuniştilor din Republica Moldova</td>
+                        <td><span>(022) 22-32-53</span><span>primaria@gmail.com</span> </td>
+                        <td><span>bd. C. Negruzzi	</span> <span>str-la Negruzzi C.</span><span>bd. Dimitrie </span></td></tr>
+                    <tr><td>Scripnic Ivan</td><td>Partidul Comuniştilor din Republica Moldova</td>
+                        <td><span>(022) 22-32-53</span><span>primaria@gmail.com</span> </td>
+                        <td><span>bd. C. Negruzzi	</span> <span>str-la Negruzzi C.</span><span>bd. Dimitrie </span></td></tr>
+                    <tr><td>Scripnic Ivan</td><td>Partidul Comuniştilor din Republica Moldova</td>
+                        <td><span>(022) 22-32-53</span><span>primaria@gmail.com</span> </td>
+                        <td><span>bd. C. Negruzzi	</span> <span>str-la Negruzzi C.</span><span>bd. Dimitrie </span></td></tr>
+                    <tr><td>Scripnic Ivan</td><td>Partidul Comuniştilor din Republica Moldova</td>
+                        <td><span>(022) 22-32-53</span><span>primaria@gmail.com</span> </td>
+                        <td><span>bd. C. Negruzzi	</span> <span>str-la Negruzzi C.</span><span>bd. Dimitrie </span></td></tr>
+
+                </tbody>
+            </table>
+        </div>
+
+        <div class='right'>
+            <ul class="right_menu">
+                <li><a href="javascript:;">Ce este consiliul local?</a></li>
+                <li><a href="javascript:;">Ședințele Consiliului </a></li>
+                <li><a href="javascript:;">Secretarul consiliului local</a></li>
+                <li class="active"><a href="javascript:;">Consilierii locali și domeniile de responsabilitate</a></li>
+                <li><a href="javascript:;">Programul de audiență al consilierilor locali</a></li>
+                <li><a href="javascript:;">Arhiva deciziilor anterioare </a></li>
+            </ul>
+        </div>
+        <div class='clearfix'></div>
+
+
+
+        <div class='socials'>
+            <div id="vk_like"></div>
+            <div id="ok_shareWidget"></div>
+            <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-width="125" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="hr_grey"></div>
+    </div>
+
+    <div class='clearfix'></div>
+</section>
