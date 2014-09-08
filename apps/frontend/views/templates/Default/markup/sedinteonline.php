eab43b07979697e8182c87e5dd4e90bfe48ec54a
Aliona Levcenco <alyona_0594@mail.ru>
2014-09-08 15:35:40 +0300

    no message

---
+++ apps/frontend/views/templates/Default/markup/sedinteonline.php
@@ -0,0 +1,40 @@
+<section>
+    <div class="dirs_menu">
+        <div class="wrap">
+            <a href="javascript:;">Principala »</a>
+            <a href="javascript:;">Consiliul local »</a>
+            <a href="javascript:;">Consiliul local »</a>
+            <a href="javascript:;">Ședințele Consiliului » </a>
+            <a href="javascript:;">Privește on-line ședințele consiliului  </a>
+        </div>
+    </div>
+    <div class="wrap">
+        <p class="c_title">Privește on-line ședințele consiliului </p>
+
+        <div class='left'>                
+            
+        </div>
+
+        <div class='right'>
+            <ul class="right_menu">
+                <li><a href="javascript:;">Agenda următoarei ședințe</a></li>
+                <li><a href="javascript:;">Agendele ședințelor anterioare </a></li>
+                <li><a href="javascript:;">Calendarul ședințelor viitoare</a></li>
+                <li class="active"><a href="javascript:;">Privește on-line ședințele consiliului 	</a></li>
+            </ul>
+        </div>
+        <div class='clearfix'></div>
+
+
+
+        <div class='socials'>
+            <div id="vk_like"></div>
+            <div id="ok_shareWidget"></div>
+            <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-width="125" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="hr_grey"></div>
+    </div>
+
+    <div class='clearfix'></div>
+</section>
