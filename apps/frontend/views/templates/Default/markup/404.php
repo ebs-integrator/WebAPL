6f6a2e29a0dd18ca402c08f9c5c9b8da585e6f9f
Aliona Levcenco <alyona_0594@mail.ru>
2014-09-26 09:59:50 +0300

    no message

---
+++ apps/frontend/views/templates/Default/markup/404.php
@@ -0,0 +1,15 @@
+<section>
+    <div class="dirs_menu">
+        <div class="wrap">
+            <a href="#">Principala »  </a>
+            <a href="#">Rezultatul cautarii </a>
+        </div>
+    </div>
+    <div class="wrap">
+        <img src="<?=res('assets/img/404-ERROR.png');?>" class="img_error">
+        <div class="error_info">
+            <p><span>Eroare: </span>Ne pare rău,</p>
+            <p>pagina solicitată,nu a putut fi găsită!</p>
+        </div>
+    </div>
+</section>
