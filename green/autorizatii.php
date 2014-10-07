0fbfde3155d4b944f9b5fadb014dedc84001ca76
Aliona Levcenco <alyona_0594@mail.ru>
2014-10-07 11:54:10 +0300

    no message

---
+++ green/autorizatii.php
@@ -0,0 +1,75 @@
+<?php include"blocks/page.php"; ?>
+<section>
+    <div class="wrap">
+        <div class="left_block">
+            <p class="title">Autorizații și certificate </p>
+            <ul class="menu">
+                <li class="active"><a href="javascript:;">Autorizația pentru unități comerciale</a></li>
+                <li><a href="javascript:;">Autorizația de construcție</a></li>
+                <li><a href="javascript:;">Autorizația de desființare</a></li>
+                <li><a href="javascript:;">Certificatul de urbanism pentru proiectare</a></li>
+                <li><a href="javascript:;">Certificat privind lipsa / existența restanțelor</a></li>
+                <li><a href="javascript:;">Certificat privind componenta familiei</a></li>
+                <li><a href="javascript:;">Certificate necesare pentru construcția casei sau a unui obiectiv comercial</a></li>
+                <li><a href="javascript:;">Întrebări frecvente/ Modele de cereri / Ghid de completare</a></li>
+                <li><a href="javascript:;">Ghid de completare a cererii pt. autorizatie de constructie</a></li>
+                <li><a href="javascript:;">Ghid de completare a cererii pt. certificat de urbanism</a></li>
+            </ul>
+        </div>
+        <div class="right_block">
+            <div class="dirs_menu">
+                <a href="javascript:;">Principala »</a>
+                <a href="javascript:;">Cetățeni și Business »</a>  
+                <a href="javascript:;"> Autorizații și certificate</a>
+            </div>
+            <div class="doc"><a href="javascript:;">Cerere eliberare acord de functionare <span class="dwnl"></span></a></div>
+
+            <table class="atz">
+                <tbody>
+                    <tr><td>1.</td><td>Scopul serviciului public</td><td><p>Autorizaţia de funcționare este un act administrativ individual ce atestă dreptul titularului autorizaţiei de a desfăşura activitatea în unitatea comercială şi/sau de prestări servicii în locul (terenul, localul, incinta) stabilit, pentru o perioadă stabilită, cu respectarea obligatorie a condiţiilor de autorizare prevăzute de legislaţie.</p>
+                        </td></tr>
+                    <tr><td>2.</td><td>Organul emitent. Subdiviziunea organului emitent responsabilă de prestarea serviciului. Datele de contact si programul de lucru. </td>
+                        <td>
+                            <p>Primăria or. Orhei, et.1, bir. 4.</p>
+                            <p>Specialist Reglementare Probleme de Comerţ,</p>
+                            <p>Apreutesei Marcel</p>
+                            <p>Tel: 0(235)-22771</p>
+                            <p>Email: <a href="mailto:autorizatii-functionare@orhei.md">autorizatii-functionare@orhei.md</a></p>
+                            <p>Programul de lucru: Luni – Vineri, 08:00 – 17:00</p>
+                        </td></tr>
+                    <tr><td>3.</td><td>Planul încăperii nelocative actualizat</td><td><ol>
+                                <li><a href="http://apl.localhost/page/markup/autorizatii">Cerere tipzată (descarcă)</a></li>
+                                <li>Extrasul din Registrul de stat al persoanelor juridice și întreprinzătorilor individuali;</li>
+                                <li>Certificatul de proprietate asupra imobilului cu destinaţie nelocativă, eliberat de către Oficiul cadastral teritorial. Pentru unităţile amplasate în încăperi închiriate urmează să fie prezentat şi contractul de locaţiune;</li>
+                                <li>Planul încăperii nelocative actualizat la OCT;</li>
+                                <li>Schema de amplasare a unităţii comerciale (pentru gherete, tarabe şi alte unităţi mobile);</li>
+                                <li>Contractul de locaţiune sau titlul de autentificare a deţinătorului de teren;</li>
+                                <li>Autorizaţia sanitară de funcţionare, pentru toate tipurile de unităţi comerciale şi/sau prestare servicii, iar în cazul autorizării magazinelor alimentare şi unităţilor de alimentaţie publică suplimentar şi asortimentul produselor comercializate; Autorizaţia sanitară veterinară de funcţionare</li>
+                            </ol>
+                        </td></tr>
+                    <tr><td>4.</td><td>Scopul serviciului public</td><td>
+                            <p> Autorizaţia de funcționare este un act administrativ individual ce atestă dreptul titularului autorizaţiei de a desfăşura activitatea în unitatea comercială şi/sau de prestări servicii în locul (terenul, localul, incinta) stabilit, pentru o perioadă stabilită, cu respectarea obligatorie a condiţiilor de autorizare prevăzute de legislaţie. </p>
+                        </td></tr>
+                </tbody>
+            </table>
+            <table>
+                <tr>
+                    <td>
+                        <div class="fb-like"  data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+                    </td>
+                    <td>
+                        <a href="https://twitter.com/" class="twitter-share-button" >Tweet</a>
+                    </td>
+                    <td>
+                        <div id="ok_shareWidget"></div>                       
+
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="clearfix50"></div>
+    </div>
+
+</section>
+
+<?php include"blocks/footer.php"; ?>
---
+++ green/educatie.php
@@ -0,0 +1,128 @@
+<?php include"blocks/page.php"; ?>
+<section>
+    <div class="wrap">
+        <div class="left_block">
+            <p class="title">Educație, Cultură, Turism și Sport</p>
+            <ul class="menu">
+                <li class="active"><a href="javascript:;">Grădinițe, gimnazii, licee</a></li>
+                <li><a href="javascript:;">Colegiul și Universitatea</a></li>
+                <li><a href="javascript:;">Biblioteca, Muzeul și Teatrul </a></li>
+                <li><a href="javascript:;">Parcuri, zone de odihnă și sport</a></li>
+                <li><a href="javascript:;">Atracții turistice</a></li>
+                <li><a href="javascript:;">Despre oraș  </a></li>
+            </ul>
+        </div>
+        <div class="right_block">
+            <div class="dirs_menu">
+                <a href="javascript:;">Principala »</a>
+                <a href="javascript:;">Cetățeni și Business »</a>  
+                <a href="javascript:;">Educație, Cultură, Turism și Sport »</a>
+                <a href="javascript:;">Taxe și impozite </a>
+            </div>
+            <ul class="culture">
+                <li><div class="left img">
+                        <img src="http://kopceak1.sga.webhost1.ru/upload/541ab571e4d3d_22d1ca0cc61347c29bba2961b281a6f3.png">
+                    </div>
+                    <div class="right">
+                        <p>Gradinita: MishMail</p>
+                        <div class="box">
+                            <p>mun.Chisinau,</p>
+                            <p>str.Mihai Eminescu, 55</p>
+                        </div>
+                        <div class="box">
+                            <p>tel: +373-22-202890</p>
+                            <p>+373-22-202892</p>
+                        </div>
+                        <div class="box">
+                            <p>fax: +373-22-226140</p>
+                            <p>
+                                <a target="_blank" href="web: www.satiricus.md">web:  web: www.satiricus.md</a>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li><div class="left img">
+                        <img src="http://kopceak1.sga.webhost1.ru/upload/541ab5c6c752e_4301f6c7eab2b6154b04553f8eea55f5.png">
+                    </div>
+                    <div class="right">
+                        <p>Gradinita: Tom si Jerry</p>
+                        <div class="box">
+                            <p>mun.Chisinau,</p>
+                            <p>bd Traian 2/1</p>
+                        </div>
+                        <div class="box">
+                            <p>tel:  +373-22-202890</p>
+                            <p> +373-22-202890</p>
+                        </div>
+                        <div class="box">
+                            <p>fax:  +373-22-226140;</p>
+                            <p>
+                                <a target="_blank" href="web: www.satiricus.md">web:  web: www.satiricus.md</a>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li><div class="left img">
+                        <img src="http://kopceak1.sga.webhost1.ru/upload/5419b6f1a0886_f423343082b5710ff3438d914c9874b1.png">
+                    </div>
+                    <div class="right">
+                        <p>Regia apa canal</p>
+                        <div class="box">
+                            <p>mun. Chisinau</p>
+                            <p>bd Traian 2/1</p>
+                        </div>
+                        <div class="box">
+                            <p>tel: 067 343 232</p>
+                            <p>022 543 123</p>
+                        </div>
+                        <div class="box">
+                            <p>fax: 0345 234 235</p>
+                            <p>
+                                <a target="_blank" href="http://ebs.md/">web:  http://ebs.md/</a>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li><div class="left img">
+                        <img src="http://kopceak1.sga.webhost1.ru/upload/54007d9a65b87_b3b4c278e60946919e3741f5a7869eae.jpg">
+                    </div>
+                    <div class="right">
+                        <p>Super title4t5gcrvg</p>
+                        <div class="box">
+                            <p>adresa 1</p>
+                            <p>adresa 2</p>
+                        </div>
+                        <div class="box">
+                            <p>tel: 876 345 234</p>
+                            <p>098 54654 34</p>
+                        </div>
+                        <div class="box">
+                            <p>fax: 0987 453 34224</p>
+                            <p>
+                                <a target="_blank" href="google.com">web:  google.com</a>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <table>
+                <tr>
+                    <td>
+                        <div class="fb-like"  data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+                    </td>
+                    <td>
+                        <a href="https://twitter.com/" class="twitter-share-button" >Tweet</a>
+                    </td>
+                    <td>
+                        <div id="ok_shareWidget"></div>                       
+
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="clearfix50"></div>
+    </div>
+
+</section>
+
+<?php include"blocks/footer.php"; ?>
