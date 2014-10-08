b216dbafb759afd1f209b89ad6e17d9b31967223
Aliona Levcenco <alyona_0594@mail.ru>
2014-10-08 12:05:54 +0300

    no message

---
+++ green/faq_model_ghid.php
@@ -0,0 +1,54 @@
+<?php include"blocks/page.php"; ?>
+<section>
+    <div class="wrap">
+        <div class="left_block">
+            <p class="title">Autorizații și certificate</p>
+            <ul class="menu">
+                <li><a href="javascript:;">Autorizația pentru unități comerciale</a></li>
+                <li><a href="javascript:;">Autorizația de construcție</a></li>
+                <li><a href="javascript:;">Autorizația de desființare</a></li>
+                <li><a href="javascript:;">Certificatul de urbanism pentru proiectare</a></li>
+                <li><a href="javascript:;">Certificat privind lipsa / existența restanțelor</a></li>
+                <li><a href="javascript:;">Certificat privind componenta familiei</a></li>
+                <li><a href="javascript:;">Certificate necesare pentru construcția casei sau a unui obiectiv comercial</a></li>
+                <li class="active"><a href="javascript:;">Întrebări frecvente/ Modele de cereri / Ghid de completare</a></li>
+                <li><a href="javascript:;">Ghid de completare a cererii pt. autorizatie de constructie</a></li>
+                <li><a href="javascript:;">Ghid de completare a cererii pt. certificat de urbanism</a></li>
+            </ul>
+        </div>
+        <div class="right_block">
+            <div class="dirs_menu">
+                <a href="javascript:;">Principala »</a>
+                <a href="javascript:;">Cetățeni și Business »</a>  
+                <a href="javascript:;">Autorizații și certificate »</a>
+                <a href="javascript:;">Întrebări frecvente/ Modele de cereri / Ghid de completare </a>
+            </div>
+            <nav>
+                <a href="javascript:;">
+                    <p>Întrebări frecvente</p>
+                    <span>Daca aveti intrebari referirtor la utilizarea sute-ului va rugat sa accesatzi meniul dat</span>
+                    <div class="more"></div>
+                </a>
+            </nav>
+            <nav>
+                <a href="javascript:;">
+                    <p>Modele de cereri</p>
+                    <span>Promit să construiesc case pentru sute de familii. </span>
+                    <div class="more"></div>
+                </a>
+            </nav>
+            <nav>
+                <a href="javascript:;">
+                    <p>Ghid de completare</p>
+                    <span>Promit să fac din acest oras alegerea preferată pentru investiții și crearea de locuri de muncă. </span>
+                    <div class="more"></div>
+                </a>
+            </nav>
+
+        </div>
+        <div class="clearfix50"></div>
+    </div>
+
+</section>
+
+<?php include"blocks/footer.php"; ?>
