098bcb67ed2e20b92b932e6a20e6891086c1cf45
Aliona Levcenco <alyona_0594@mail.ru>
2014-10-11 14:57:20 +0300

    no message

--- green/proiectlocal.php
+++ green/proiectlocal.php
@@ -16,33 +16,52 @@
                 <a href="javascript:;">Proiectele Locale »</a>                
                 <a href="javascript:;">Proiecte finalizate</a>
             </div>
-
-            <table class="plc">
+            <table class="plcu">
+                <thead>
+                    <tr><th>Denumire proiect</th><td>„DRUMUL R33 PĂȘCANI-SECĂRENI-BOLȚUN - M1 – DRUM AL SPERANȚEI DE RELANSARE ECONOMICĂ REGIONALĂ”</td></tr>
+                </thead>
                 <tbody>
-                    <tr><td>1.</td>
-                        <td><a href="javascript:;">ghgfhbnvbcn</a></td>
+                    <tr><th>Data de start și finalizare	</th><td>intervallgfsdg sdfgdfg sdf</td></tr>
+                    <tr><th>Domeniu	</th><td><p>11111f gbvcbxcv bvc bxvcb fsh v nvcvbn gbx fd bxvbx cvb xcvb vcb xvbxvcb xcvbxcv bxct hg nvbnvcnbn</p>
+                        </td></tr>
+                    <tr><th>Stadiul de realizare proiect</th><td>222in asteptare</td></tr>
+                    <tr><th>Valoarea proiectului</th><td>333321312313 lei</td></tr>
+                    <tr><th>Parteneri	</th><td><p>4444fhdgd hhgfd gdfsgsdgsdfdfg hdfghdfg</p>
+                        </td></tr>
+                    <tr><th>Relevanța și impactul regional al proiectului</th><td><p>gh gncvbncvn vbnvvbncvb hfbvc bvcb xcvbcvbxcvbxcvbxcvbxcncv n</p>
+                        </td></tr>
+                    <tr><th>Activități principale</th><td><p>55555</p>
+                        </td></tr>
+                    <tr><th>Produsele proiectului	</th><td><p>66666</p>
+                        </td></tr>
+                    <tr><th>Rezultatele proiectului	</th><td><p>77777</p>
+                        </td></tr>
+                    <tr><th>Obiectivele proiectului</th><td><p>88888</p>
+                        </td></tr>
+                    <tr><th>Valoarea totală a proiectului</th><td><table border="1" cellpadding="1" cellspacing="1" style="width:500px">
+                                <tbody>
+                                    <tr>
+                                        <td>vbx cvb cxv</td>
+                                        <td>cvb xcvbvcbxcv</td>
+                                    </tr>
+                                    <tr>
+                                        <td>cvb xvcbcvb</td>
+                                        <td>&nbsp;xvcb cvbx xcvb</td>
+                                    </tr>
+                                    <tr>
+                                        <td>vc bxvcbxcv bxcvbxcv&nbsp;</td>
+                                        <td>cbxcvbxvcb xcv bxcv</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <p>&nbsp;</p>
+                        </td></tr>
+                    <tr>
+                        <th>Fișiere</th>
                         <td>
-                            <table>
-                                <tbody><tr><td>Data de start și finalizare:</td><td>intervallgfsdg sdfgdfg sdf</td></tr>
-                                    <tr><td>Domeniu:</td><td><p>11111f gbvcbxcv bvc bxvcb fsh v nvcvbn gbx fd bxvbx cvb xcvb vcb xvbxvcb xcvbxcv bxct hg nvbnvcnbn</p>
-                                        </td></tr>
-                                    <tr><td>Stadiul de realizare proiect:</td><td>222in asteptare</td></tr>
-                                    <tr><td>Valoarea proiectului:</td><td>333321312313 lei</td></tr>
-                                </tbody></table>
-                            <a class="more" href="javascript:;"></a>
-                        </td>
-                    </tr>
-                    <tr><td>2.</td>
-                        <td><a href="javascript:;">ghgfhbnvbcn</a></td>
-                        <td>
-                            <table>
-                                <tbody><tr><td>Data de start și finalizare:</td><td>intervallgfsdg sdfgdfg sdf</td></tr>
-                                    <tr><td>Domeniu:</td><td><p>11111f gbvcbxcv bvc bxvcb fsh v nvcvbn gbx fd bxvbx cvb xcvb vcb xvbxvcb xcvbxcv bxct hg nvbnvcnbn</p>
-                                        </td></tr>
-                                    <tr><td>Stadiul de realizare proiect:</td><td>222in asteptare</td></tr>
-                                    <tr><td>Valoarea proiectului:</td><td>333321312313 lei</td></tr>
-                                </tbody></table>
-                            <a class="more" href="javascript:;"></a>
+                            <a href="http://kopceak1.sga.webhost1.ru/upload/541007a544c50_141211f05f3044a67275c57174daee26.xls" target="_blank" class="xls">nr de angajati pe functii.xls</a>
+                            <a href="http://kopceak1.sga.webhost1.ru/upload/541007acc5247_b1711530fbc72e5f21a887894675c60d.docx" target="_blank" class="docx">structura site-urilor FINAL 28 aug.docx</a>
                         </td>
                     </tr>
                 </tbody>
