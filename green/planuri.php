a3f1da19a8baa055f94f473b95565ebd77b7ad03
Aliona Levcenco <alyona_0594@mail.ru>
2014-10-13 15:02:42 +0300

    no message

--- green/planuri.php
+++ green/planuri.php
@@ -24,12 +24,37 @@
             <div class="t_block">
                 <h2>Planurile de acţiune pentru reducerea zgomotului în raionul Strășeni</h2>
                 <p>Planurile de acţiune au fost elaborate pe baza rezultatelor conţinute de hărţile strategice de zgomot, în vederea prevenirii şi reducerii zgomotului şi a efectelor acestuia, mai cu seamă acolo unde valorile nivelurilor de zgomot depăşesc limitele admise.</p>
-                <p>După elaborarea hărţilor de zgomot s-a constatat că circulaţia rutieră are cea mai mare pondere în producerea zgomotului, de aceea şi planul de acţiune se referă în primul rând la zgomotul produs de traficul rutier.</p>
+                <h2 class="colored">1. Initiere proiect</h2>
+                <p>
+                    <span>Intalnire cu factorii de decizie locali in vederea clarificarii urmatoarelor aspecte legate de proiect:</span>
+                    <span>etapele proiectului,</span>
+                    <span>continutul acestora,</span>
+                    <span>rezultatele asteptate,</span>
+                    <span>principiile, modalitatile si calendarul de lucru,</span>
+                </p>
+                <h2 class="colored">2. Pre-audit</h2>
                 <p>Planurile de dezvoltare ale oraşului care cuprind planurile de îmbunătăţire a circulaţiei, de protecţia mediului, de urbanism, vor cuprinde şi aceste planuri de reducerea zgomotului.</p>
                 <p>Hărţile de zgomot şi Planurile de acţiune au fost predate, conform H.G. 321/ 2005, republicată, privind evaluarea şi gestionarea zgomotului ambiant, iar în concordanţă cu  art.4, aliniat 9, acestea vor fi actualizate şi predate Agenţiei Regionale de Protecţia Mediului, o dată la 5 ani, respectiv 30 iunie 2012 pentru Hărţile de zgomot şi 18 iulie 2013 pentru Planurile de acţiune.</p>
+
             </div>
-
-
+            <h1 class="reg">Articole din aceeasi categorie</h1>
+            <ul class="docs">
+                <li class="doc"><a href="javascript:;">Cerere eliberare acord de functionare <span class="dwnl"></span></a></li>
+                <li class="pdf"><a href="javascript:;">Cerere de stabilire a sumei datorate pentru ocuparea domeniului public cu cale de acces  <span class="dwnl"></span></a></li>
+                <li class="xls"><a href="javascript:;">Cerere stabilire sume pentru ocuparea domeniului public cu ghereta  <span class="dwnl"></span></a></li>
+                <li class="doc"><a href="javascript:;">Cerere eliberare acord de functionare <span class="dwnl"></span></a></li>
+                <li class="doc"><a href="javascript:;">Cerere de stabilire a sumei datorate pentru ocuparea domeniului public cu cale de acces  <span class="dwnl"></span></a></li>
+                <li class="xlsx"><a href="javascript:;">Cerere eliberare acord de functionare <span class="dwnl"></span></a></li>
+                <li class="pdf"><a href="javascript:;">Cerere stabilire sume pentru ocuparea domeniului public cu ghereta  <span class="dwnl"></span></a></li>
+                <li class="pdf"><a href="javascript:;">Cerere de stabilire a sumei datorate pentru ocuparea domeniului public cu cale de acces  <span class="dwnl"></span></a></li>
+                <li class="doc"><a href="javascript:;">Cerere eliberare acord de functionare <span class="dwnl"></span></a></li>
+                <li class="doc"><a href="javascript:;">Cerere eliberare acord de functionare <span class="dwnl"></span></a></li>
+                <li class="doc"><a href="javascript:;">Cerere de stabilire a sumei datorate pentru ocuparea domeniului public cu cale de acces  <span class="dwnl"></span></a></li>
+                <li class="xlsx"><a href="javascript:;">Cerere eliberare acord de functionare <span class="dwnl"></span></a></li>
+                <li class="pdf"><a href="javascript:;">Cerere stabilire sume pentru ocuparea domeniului public cu ghereta  <span class="dwnl"></span></a></li>
+                <li class="pdf"><a href="javascript:;">Cerere de stabilire a sumei datorate pentru ocuparea domeniului public cu cale de acces  <span class="dwnl"></span></a></li>
+                <li class="doc"><a href="javascript:;">Cerere eliberare acord de functionare <span class="dwnl"></span></a></li>
+            </ul>
 
         </div>
     </div>
