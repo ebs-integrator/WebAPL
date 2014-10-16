37d76b6c13dd87af62623e4eed75532d4eeb51b6
Godina Nicolae <ngodina@ebs.md>
2014-10-16 17:08:33 +0300

    no message

--- apps/frontend/views/templates/Flat/sections/pages/blocks/files.php
+++ apps/frontend/views/templates/Flat/sections/pages/blocks/files.php
@@ -3,14 +3,14 @@
         <?php if ($page->show_file_search) { ?>
             <div class="search">
                 <input class="search_input" type="text" placeholder="<?= varlang('modelul-formularul'); ?>">
-                <input class="search_start" type="button" value="<?= varlang('search'); ?>">
+                <input class="search_start" type="submit" value="<?= varlang('search'); ?>">
                 <div class="clearfix"></div>
             </div>
         <?php } ?>
-        <ul class="mda">
+        <ul class="docs">
             <?php foreach ($page->files as $file) { ?>
             <li class="<?=$file->extension;?>">
-                <span><a href="<?=url($file->path);?>" target="_block"><?=$file->name;?></a></span><a href="<?=url($file->path);?>" target="_block" class="active"></a>
+                <a href="<?=url($file->path);?>" target="_block"><?=$file->name;?></a>
             </li>
             <?php } ?>
         </ul>
