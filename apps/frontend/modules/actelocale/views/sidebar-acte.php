6866ad5da8d74a68c08a4d6fcd7f46c7b243e827
Aliona Levcenco <alyona_0594@mail.ru>
2014-10-21 19:12:42 +0300

    no message

--- apps/frontend/modules/actelocale/views/sidebar-acte.php
+++ apps/frontend/modules/actelocale/views/sidebar-acte.php
@@ -1,14 +1,16 @@
 <article class="doc">
-    <p class="ttl"><img src="<?= res('assets/img/doc.png'); ?>"><a href="<?=Language::url('topage/acteList');?>">Toate actele locale</a></p>
+    <p class="ttl"><img src="<?= res('assets/img/doc.png'); ?>"><a href="<?= Language::url('topage/acteList'); ?>">Toate actele locale</a></p>
     <div class="hr"></div>
     <table>
         <tbody>
             <?php foreach ($acte as $act) { ?>
-            <tr>
-                <td><?=$act->doc_nr;?></td><td><a target="_blank" href="<?=url($act->path);?>"><?=$act->title;?></a></td>
-            </tr>
+                <tr>
+                    <td><?= $act->doc_nr; ?></td><td><a target="_blank" href="<?= url($act->path); ?>"><?= $act->title; ?></a></td>
+                </tr>
             <?php } ?>
         </tbody>
     </table>
+
+    <a href="javascript:;" class="more"></a>
     <div class="clearfix10"></div>
 </article>
