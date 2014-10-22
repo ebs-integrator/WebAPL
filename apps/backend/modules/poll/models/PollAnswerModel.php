9876ace2d90c3c486830223b93e9faafe31b284f
Godina Nicolae <ngodina@ebs.md>
2014-10-22 17:32:24 +0300

    poll backend

--- apps/backend/modules/poll/models/PollAnswerModel.php
+++ apps/backend/modules/poll/models/PollAnswerModel.php
@@ -2,6 +2,8 @@
 
 class PollAnswerModel extends Eloquent {
 
+    use EloquentTrait;
+    
     protected $table = 'apl_poll_answer';
     public static $ftable = 'apl_poll_answer'; // public table name
     public $timestamps = false;
---
+++ apps/backend/modules/poll/views/tab-answers.php
@@ -0,0 +1,39 @@
+
+<script>
+    var poll_<?= ( isset($poll->id) ? $poll->id : '') ?>_options = {
+        url: '<?= url('poll/list/answer/' . ( isset($poll->id) ? $poll->id : '')) ?>',
+        datatype: "json",
+        mtype: 'POST',
+        autoencode: true,
+        loadonce: false,
+        colNames: ['ID', '<?= varlang('answer'); ?>'],
+        colModel: [
+            {name: 'id', index: 'id', hidden: true, editable: false, editoptions: {readonly: true, size: 10}, width: 10},
+            {name: 'answer', index: 'answer', height: 50, resizable: true, align: "left", editable: true, edittype: "text"}
+        ],
+        rowNum: 30,
+        multiselect: false,
+        rowList: [30, 50, 100, 200, 500],
+        pager: '',
+        altRows: true,
+        sortname: 'id',
+        viewrecords: true,
+        sortorder: "asc",
+        height: $(window).height() * 0.7,
+        width: $('#content').width() - 70,
+        caption: "",
+        editurl: '',
+        loadComplete: function() {
+            $("#pager-poll_we_left table").hide();
+        },
+        onSelectRow: function(rowid) {
+            window.location.href = '<?= url('poll/answer/edit') ?>/'+rowid;
+        }
+    };
+</script>
+
+<div class="c20"></div>
+<a class="btn btn-success" href="<?=url('poll/anwser/create/' . $poll->id);?>">Adauga un raspuns</a>
+<div class="c20"></div>
+
+<?= View::make('sections/jqgrid/form')->with('options', 'poll_' . ( isset($poll->id) ? $poll->id : '') . '_options')->with('id', 'poll_we'); ?>
