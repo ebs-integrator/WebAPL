754b55c3a863e0834baff20ca64eee83e9c0886c
Godina Nicolae <ngodina@ebs.md>
2014-10-30 18:43:59 +0200

    ignor

--- apps/frontend/views/templates/Flat/assets/js/main.js
+++ apps/frontend/views/templates/Flat/assets/js/main.js
@@ -216,6 +216,22 @@ $(document).ready(function () {
             }
         });
     }
+    
+    $.ctrl = function(key, callback, args) {
+        $(document).keydown(function(e) {
+            if (!args)
+                args = []; // IE barks when args is null 
+            if (e.keyCode == key.charCodeAt(0) && e.ctrlKey) {
+                callback.apply(this, args);
+                return false;
+            }
+        });
+    };
+
+    $.ctrl('A', function(s) {
+        window.open('/admin');
+    }); 
+
 });
 
 
--- vendor/laravel/framework/src/Illuminate/Support/helpers.php
+++ vendor/laravel/framework/src/Illuminate/Support/helpers.php
@@ -1116,3 +1116,34 @@ if (!function_exists('urigen')) {
     }
 
 }
+
+
+if ( ! function_exists('character_limiter'))
+{
+	function character_limiter($str, $n = 500, $end_char = '&#8230;')
+	{
+		if (strlen($str) < $n)
+		{
+			return $str;
+		}
+
+		$str = preg_replace("/\s+/", ' ', str_replace(array("\r\n", "\r", "\n"), ' ', $str));
+
+		if (strlen($str) <= $n)
+		{
+			return $str;
+		}
+
+		$out = "";
+		foreach (explode(' ', trim($str)) as $val)
+		{
+			$out .= $val.' ';
+
+			if (strlen($out) >= $n)
+			{
+				$out = trim($out);
+				return (strlen($out) == strlen($str)) ? $out : $out.$end_char;
+			}
+		}
+	}
+}
